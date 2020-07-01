8d181037649df18d9dc9c8805148e7ce51325f5f
Edditoria <edditoria@gmail.com>
2020-07-02 01:06:20 +0800

    Add health check

--- backup.cmd
+++ backup.cmd
@@ -4,12 +4,30 @@
 set "nul=nul"
 if "%OS%"=="Windows_NT" set "nul="
 
+
 :: Set variables
 
 set "nativelang_dir=%USERPROFILE%\AppData\Roaming\Notepad++\"
 set "nativelang_file=nativeLang.xml"
 set "nativelang_full_path=%nativelang_dir%%nativelang_file%"
 set "backup_dir=%nativelang_dir%\backup\"
+
+
+:: Health check
+
+if not exist "%nativelang_dir%%nul%" (
+	echo [error] Directory not found: %nativelang_dir%
+	exit /b 1
+)
+if not exist "%nativelang_full_path%" (
+	echo [error] File not found: %nativelang_file%
+	exit /b 1
+)
+if not exist "%backup_dir%%nul%" (
+	echo [error] Directory not found: %backup_dir%
+	exit /b 1
+)
+
 
 :: Temp Output
 
